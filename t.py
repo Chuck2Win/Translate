693223572333f117876c0ddedf9594d9b34901fc
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-09-28 16:41:59 +0900

    Update t.py

--- t.py
+++ t.py
@@ -1,3 +1,10 @@
+# candidates - batch size, num_beams, 2(list, score)
+# single beam search 
+
+# single beam search
+
+
+
 i = 0
 with torch.no_grad():
     model.eval()
@@ -7,6 +14,7 @@ for b in range(len(outputs)): # batch
     for k in range(num_beams): # num_beams
         candidates[b][k][0].append(c.indices[k].item())
         candidates[b][k][1]+=(c.values[k].item())
+
 def make_batch(candidates):
     # candidates shape : (batch size, num_beams, 2) ~ list, log prob
     result = []
@@ -17,19 +25,21 @@ def make_batch(candidates):
             batch[b][1:length+1] = candidates[b][i][0]
         result.append(batch)
     return result
-
+# x ~ batch size, num_beams, seq_len
+# y ~ batch size, num_beams, seq_len
 x = make_batch(candidates)
 x = torch.LongTensor(x).transpose(0,1).reshape(-1, max_length).to(args.device)
 y = encoder_input_ids.unsqueeze(1).repeat(1,num_beams,1).reshape(-1,args.seq_len_1) # batch_size*num_beams, max_length
-#for i in range(1,max_length):
-with torch.no_grad():
-    model.eval()
-    outputs = model.forward(y.to(args.device), x.to(args.device)) # bs, seq_len_2, n_vocab
-outputs = outputs.reshape(args.batch_size, num_beams, max_length, -1)
-outputs[0,:,1,:].shape
-for b in range(args.batch_size): # batch
-    c  = outputs[b][i].topk(num_beams,dim=-1)
-    for k in range(num_beams): # num_beams
-        candidates[b][k][0].append(c.indices[k].item())
-        candidates[b][k][1]+=(c.values[k].item())
+
+for i in range(1,max_length):
+    with torch.no_grad():
+        model.eval()
+        outputs = model.forward(y.to(args.device), x.to(args.device)) # bs, seq_len_2, n_vocab
+    # outputs ~ batch size * num_beams, seq_len, n_vocab
+    outputs = outputs.reshape(args.batch_size, num_beams, max_length, -1) # batch size, num_beams, seq_len, n_vocab
+    for b in range(args.batch_size): # batch
+        c  = outputs[b,:,i,:].topk(num_beams,dim=-1)
+        for k in range(num_beams): # num_beams
+            candidates[b][k][0].append(c.indices[k].item())
+            candidates[b][k][1]+=(c.values[k].item())
 outputs
